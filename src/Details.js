e86d6bbf957e89177d90254ecd5ebe97fde2ef44
maxdouglasb <bmaxdouglas@gmail.com>
2024-05-15 16:35:49 -0300

    portifolio

---
+++ src/Details.js
@@ -0,0 +1,164 @@
+// Enter all your detials in this file
+// Logo images
+
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+
+// Enter your Personal Details here
+export const personalDetails = {
+    name: "Maxdouglas Borges",
+    tagline: "I build things for web",
+    img: profile,
+    about: `Front end developer passionate about technology and constant personal and professional development.
+    Throughout my career, I had the opportunity to work as a Test QA performing automated and integrated tests. I also have experience in Front end projects, using technologies such as HTML, CSS, JavaScript, React
+    
+    In addition to my passion for technology, I am committed, dedicated, communicative, with great interpersonal skills and I enjoy working in a team. I have ease of learning and I am always up to date on trends and advances in the area of ​​development.
+    
+    My professional goal is to increasingly improve my knowledge in FullStack, with a special focus on Back end. I'm always looking for exciting challenges that allow me to grow and contribute to innovative projects.
+    
+    In addition to improving technical skills, my professional experience also allows me to develop personal skills, such as collaborative intelligence, good relationships with co-workers and clients, good feedback flexibility, resilience, critical analysis and problem solving `,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+    linkdein: "https://www.linkedin.com/in/maxdouglasb/",
+    github: "https://www.github.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [{
+        Position: " Technical Support",
+        Company: ` Memora Innovative Processes SA`,
+        Location: "Goiania",
+        Type: "Full Time",
+        Duration: "November 2023 - May 2024",
+    },
+    {
+        Position: "Customer Service Representative",
+        Company: `Lotteries Look`,
+        Location: "Goiania",
+        Type: "Full Time",
+        Duration: "June 2015 - March 2023",
+    },
+];
+
+// Enter your Education Details here
+export const eduDetails = [{
+        Position: "Frontend Development",
+        Company: "Dev Club",
+        Location: "Online",
+        Type: "Full Time",
+        Duration: "Jan 2022 - Present",
+    },
+    {
+        Position: "Bachelor in systems analysis",
+        Company: `Goias university center`,
+        Location: "Goiania",
+        Type: "Full Time",
+        Duration: "January 2017 - December 2023",
+    },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+    html: html,
+    css: css,
+    js: js,
+    react: react,
+    redux: redux,
+    sass: sass,
+    tailwind: tailwind,
+    bootstrap: bootstrap,
+    vscode: vscode,
+    postman: postman,
+    npm: npm,
+    git: git,
+    github: github,
+    figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [{
+        title: "PROJECT TASK LIST",
+        image: projectImage1,
+        description: `Project developed to improve development techniques adds a new task to the list, deletes and changes statuses by changing colors`,
+        techstack: "HTML/CSS, JavaScript/React",
+        previewLink: "https://google.com",
+        githubLink: "https://github.com/maxdouglasb/projetoReactInicio",
+    },
+    {
+        title: "Project title 2",
+        image: projectImage2,
+        description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+        techstack: "HTML/CSS, JavaScript",
+        previewLink: "https://google.com",
+        githubLink: "https://github.com",
+    },
+    {
+        title: "Project title 3",
+        image: projectImage3,
+        description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+        techstack: "HTML/CSS, JavaScript",
+        previewLink: "https://google.com",
+        githubLink: "https://github.com",
+    },
+    {
+        title: "Project title 4",
+        image: projectImage4,
+        description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+        techstack: "HTML/CSS, JavaScript",
+        previewLink: "https://google.com",
+        githubLink: "https://github.com",
+    },
+    {
+        title: "Project title 5",
+        image: projectImage5,
+        description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+        techstack: "HTML/CSS, JavaScript",
+        previewLink: "https://google.com",
+        githubLink: "https://github.com",
+    },
+    {
+        title: "Project title 6",
+        image: projectImage6,
+        description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+        techstack: "HTML/CSS, JavaScript",
+        previewLink: "https://google.com",
+        githubLink: "https://github.com",
+    },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+    email: "desenvolvedor.maxdouglas@gmail.com",
+    phone: "+55 062 99916-3890",
+};
